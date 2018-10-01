2fd27aa2c573b0b53c9219c4146554efed4c2718
Giuseppe Pellegrino <peppe95.pellegrino@gmail.com>
2018-10-01 15:19:22 +0200

    config

--- src/enigma_component/Configuration.java
+++ src/enigma_component/Configuration.java
@@ -11,6 +11,32 @@ package enigma_component;
  * @author peppepel95
  */
 public class Configuration {
-    private int[] posRotori;
-    
+    private int[] Rot;
+    private int[] StartPos;
+    private String[] Plugboard;
+    private String[] Reflector;
+
+    public void setRot(String[] Rot) {
+        int[] temp = null;
+        for (int i = 0; i < Rot.length; i++) {
+            temp[i] = Integer.parseInt(Rot[i]);
+        }
+        this.Rot = temp;
+    }
+
+    public void setStartPos(String[] StartPos) {
+        int[] temp = null;
+        for (int i = 0; i < StartPos.length; i++) {
+            temp[i] = Integer.parseInt(StartPos[i]);
+        }
+        this.StartPos = temp;
+    }
+
+    public void setPlugboard(String[] Plugboard) {
+        this.Plugboard = Plugboard;
+    }
+
+    public void setReflector(String[] Reflector) {
+        this.Reflector = Reflector;
+    }
 }
--- src/enigma_utilities/Read_input_file.java
+++ src/enigma_utilities/Read_input_file.java
@@ -21,6 +21,7 @@ public class Read_input_file {
     private final Riflettore riflettore;
     private final String rot;
     private final String config;
+    private Configuration config_obj;
 
     public Read_input_file(String rot, String config) {
         this.rot = rot;
@@ -30,7 +31,7 @@ public class Read_input_file {
         this.riflettore = null;
     }
     
-    private void readText(String file, String type) throws FileNotFoundException, IOException {
+    private void readText(String file, String type) throws FileNotFoundException, IOException, Exception {
         String result;
         boolean eof = false;
         FileReader filein = new FileReader(file + ".txt");
@@ -46,51 +47,72 @@ public class Read_input_file {
         }
     }
 
-    private void setConfiguration() throws IOException {
+    private void setConfiguration() throws IOException, Exception {
         this.readText(this.rot, "setConfiguration");
     }
     
-    private void setRotori() throws IOException {
+    private void setRotori() throws IOException, Exception {
         this.readText(this.rot, "setRotori");
     }
 
-    public ArrayList<Rotore> getRotori() throws IOException {
+    public ArrayList<Rotore> getRotori() throws IOException, Exception {
         if (this.rotori.isEmpty())
             this.setRotori();
         return rotori;
     }
 
-    public Scambiatore getScambiatore() throws IOException {
+    public Scambiatore getScambiatore() throws IOException, Exception {
         if (this.scambiatore == null)
             this.setConfiguration();
         return scambiatore;
     }
 
-    public Riflettore getRiflettore() throws IOException {
+    public Riflettore getRiflettore() throws IOException, Exception {
         if (this.riflettore == null)
             this.setConfiguration();
         return riflettore;
     }
 
-    private void hook(String type, String result) {
+    private void hook(String type, String result) throws Exception {
+        String pattern;
+        String[] str;
+        
         if (type.equals("setRotori")) {
             if (!result.contains("Rotore")) {
                 rotori.add(new Rotore());
             }
         }
         if (type.equals("setConfiguration")) {
-            String[] str = result.split(": ");
+            str = result.split(": ");
             if (str[1].contains(",")) {
-                String[] items = str[1].split(", ");
+                pattern = ", ";
             }
             else {
-                String[] items = str[1].split(" ");
+                pattern = " ";
+            }
+            String[] items = str[1].split(pattern);
+            this.config_obj = new Configuration();
+            switch (str[0]) {
+                case "Rotori":
+                    config_obj.setRot(items);
+                    break;
+                case "Posizione iniziale":
+                    config_obj.setStartPos(items);
+                    break;
+                case "Scambiatore":
+                    config_obj.setPlugboard(items);
+                    break;
+                case "Riflettore":
+                    config_obj.setReflector(items);
+                    break;
+                default:
+                    throw new Exception();
             }
         }
     }
     
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         Read_input_file rif = new Read_input_file("rotore", "configurazione");
         ArrayList<Rotore> rotori = rif.getRotori();
     }
