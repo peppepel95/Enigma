759c56e4abe15ddcd74710127748c3285e047149
Giuseppe Pellegrino <peppe95.pellegrino@gmail.com>
2018-10-01 08:58:41 +0200

    classes

---
+++ src/enigma_main/Enigma.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enigma_main;
+
+/**
+ *
+ * @author peppepel95
+ */
+public class Enigma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
