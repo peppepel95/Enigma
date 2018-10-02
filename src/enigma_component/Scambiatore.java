7cb6aae63c9561d6b930c78431f75f8a43892046
Giuseppe Pellegrino <peppe95.pellegrino@gmail.com>
2018-10-02 16:20:04 +0200

    scambiatore e riflettore

--- src/enigma_component/Scambiatore.java
+++ src/enigma_component/Scambiatore.java
@@ -9,19 +9,14 @@ package enigma_component;
  *
  * @author peppepel95
  */
-public class Scambiatore {
+public class Scambiatore extends Riflettore{
     private String  plugboard;
-    
-    public void setPlugboard(String plugboard) {
-        this.plugboard = plugboard;
-    }
 
-    public String getPlugboard() {
-        return plugboard;
+    public Scambiatore(String[] reflector) {
+        super(reflector);
     }
     
-    public char translate(char character) {
-        int index = (((int) character) - 65) % 26;
-        return plugboard.charAt(index);
+    public char Swap(char character) {
+        return super.reflect(character);
     } 
 }
