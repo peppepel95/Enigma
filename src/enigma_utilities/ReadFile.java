a71b81ce4e664123d22d0aff4f1d96d5fa38b2a1
ivanColucci <32535415+ivanColucci@users.noreply.github.com>
2018-10-14 10:24:43 +0200

    Modifica Eccezioni Annulla Revert

--- src/enigma_utilities/ReadFile.java
+++ src/enigma_utilities/ReadFile.java
@@ -9,28 +9,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author peppepel95
  */
 public abstract class ReadFile {
-    
-    public void readText(String file, String type) throws FileNotFoundException, IOException, Exception {
+
+    public void readText(String file, String type) {
         String result;
         boolean eof = false;
-        FileReader filein = new FileReader(file + ".txt");
-        BufferedReader br = new BufferedReader(filein);
-        while (!eof) {
-            result = br.readLine();
-            if (result == null) {
-                eof = true;
+        FileReader filein;
+        try {
+            filein = new FileReader(file + ".txt");
+            BufferedReader br = new BufferedReader(filein);
+            while (!eof) {
+                result = br.readLine();
+                if (result == null) {
+                    eof = true;
+                } else {
+                    this.hook(type, result);
+                }
             }
-            else {
-                this.hook(type,result);
-            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(Arrays.toString(ex.getStackTrace()));
+            JOptionPane.showMessageDialog(null, "File non trovato!");
+        } catch (IOException ex) {
+             System.err.println(Arrays.toString(ex.getStackTrace()));
+            JOptionPane.showMessageDialog(null, "File non trovato!");
         }
     }
 
-    public abstract void hook(String type, String result) throws Exception;
+    public abstract void hook(String type, String result) throws IllegalArgumentException;
 }
